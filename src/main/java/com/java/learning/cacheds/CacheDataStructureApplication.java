b1b30d3a7d0886aca4335e8a62fc10499a771b86
Vimlesh <vimlesh.kumar@osttra.com>
2024-06-08 16:34:27 +0530

    cache ds implementation

---
+++ src/main/java/com/java/learning/cacheds/CacheDataStructureApplication.java
@@ -0,0 +1,11 @@
+package com.java.learning.cacheds;
+
+public class CacheDataStructureApplication {
+    public static void main(String[] args) {
+        CacheDataStructure<String, Integer> cache = new CacheDataStructure<>();
+        cache.put("Vimlesh", 123);
+        cache.put("Akash", 124);
+        cache.put("XYZ", 111);
+        System.out.println(cache.get("Vimlesh"));
+    }
+}
---
+++ src/main/java/com/java/learning/cacheds/DoublyLinkedList.java
@@ -0,0 +1,38 @@
+package com.java.learning.cacheds;
+
+class DoublyLinkedList<K, V> {
+    private Node<K, V> head;
+    private Node<K, V> tail;
+
+    public DoublyLinkedList() {
+        this.head = new Node<>(null, null);
+        this.tail = new Node<>(null, null);
+        head.setNext(tail);
+        tail.setPrev(head);
+    }
+
+    public void moveToHead(Node<K, V> node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    public void addToHead(Node<K, V> node) {
+        node.setNext(head.getNext());
+        node.setPrev(head);
+        head.getNext().setPrev(node);
+        head.setNext(node);
+    }
+
+    public void removeNode(Node<K, V> node) {
+        node.getPrev().setNext(node.getNext());
+        node.getNext().setPrev(node.getPrev());
+    }
+
+    public Node<K, V> removeTail() {
+        if (tail.getPrev() == head)
+            return null;
+        Node<K, V> node = tail.getPrev();
+        removeNode(node);
+        return node;
+    }
+}
