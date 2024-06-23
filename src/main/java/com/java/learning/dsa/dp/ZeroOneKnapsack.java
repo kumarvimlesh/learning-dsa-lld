59b448e9a93192b9a2087c2b99e4f41c388dbeb3
Vimlesh <vimlesh.kumar@osttra.com>
2024-06-23 22:05:52 +0530

    0/1 knapsack dp

--- src/main/java/com/java/learning/dsa/dp/ZeroOneKnapsack.java
+++ src/main/java/com/java/learning/dsa/dp/ZeroOneKnapsack.java
@@ -5,18 +5,38 @@ import java.util.Arrays;
 
 public class ZeroOneKnapsack {
     public static void main(String[] args) {
-//        int[] price = new int[]{2,6,7,4,8,9};
-//        int[] wt = new int[]{5,5,7,2,49,9};
-//        int capacity = 25;
-        ////ans = 26
+        int[] price = new int[]{2,6,7,4,8,9};
+        int[] wt = new int[]{5,5,7,2,49,9};
+        int capacity = 25;
+        //ans = 26
 
-        int[] price = new int[]{10,15,40};
-        int[] wt = new int[]{1,2,3};
-        int capacity = 6;
-        ///ans = 65
+//        int[] price = new int[]{10,15,40};
+//        int[] wt = new int[]{1,2,3};
+//        int capacity = 6;
+//        ///ans = 65
 
-        System.out.println(pickItems(wt, price, capacity));
+//        System.out.println(pickItems(wt, price, capacity));
+        System.out.println(knapsackTopToBottom(wt, price, capacity));
 
+    }
+
+    private static int knapsackTopToBottom(int[] wt, int[] price, int capacity) {
+        int[][] dp = new int[price.length+1][capacity+1];
+        for(int i = 0;i<price.length+1;i++){
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i<capacity+1;i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i=1;i<price.length+1;i++){
+            for(int j = 1;j<capacity+1;j++){
+                int profitWithoutPicking = dp[i-1][j]; //pickItem(wt, price, curCap, n+1, capacity, dp);
+                int profitWithPicking =  wt[i-1]>j? 0 : price[i-1] + dp[i-1][j-wt[i-1]];// +wt[n]>capacity? 0: price[n] + pickItem(wt, price, curCap + wt[n], n+1, capacity, dp);
+                dp[i][j] = Math.max(profitWithPicking, profitWithoutPicking);
+            }
+        }
+        return dp[price.length][capacity];
     }
 
     private static int pickItems(int[] wt, int[] price, int capacity) {
