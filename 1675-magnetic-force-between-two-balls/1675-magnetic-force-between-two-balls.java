b702c68d85666afefdb9adccee5e1e36d22aec07
Vimlesh Kumar <37749343+kumarvimlesh@users.noreply.github.com>
2025-05-28 21:10:15 +0530

    Time: 46 ms (21.73%), Space: 55.4 MB (98.48%) - LeetHub

---
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        return binarySearch(position, m);
+    }
+
+    private int binarySearch(int[] position, int m) {
+        int ans = 0;
+        int l = 1;
+        int h = position[position.length - 1] - position[0];
+        while (l<=h){
+            int mid = l + (h - l)/2;
+            if (canPlace(position, m, mid)){
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                h = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private static boolean canPlace(int[] position, int m, int midDist) {
+        int lastInd = 0, count = 1;
+        for (int i = 1; i<position.length; i++){
+            if(position[i] - position[lastInd] >= midDist){
+                count++;
+                lastInd = i;
+                if (count == m) return true;
+            }
+        }
+        return false;
+    }
+}
