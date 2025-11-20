62d468db3e78fa5d3ec88545208498525baba0fb
Vimlesh <vimlesh1.kumar@cars24.com>
2025-11-21 01:04:56 +0530

    -

--- src/main/java/com/java/learning/LearningApplication.java
+++ src/main/java/com/java/learning/LearningApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningApplication.class, args);
+
+        SpringApplication.run(LearningApplication.class, args);
 	}
 
 }
